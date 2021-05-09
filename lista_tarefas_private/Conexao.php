6e6516e56bae43057ca46fd2a216b1d703aa0214
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2021-05-09 00:21:30 -0300

    Desenvolvendo regra de negocio e reorganizando arquivos

---
+++ lista_tarefas_private/Conexao.php
@@ -0,0 +1,26 @@
+<?php
+
+class Conexao
+{
+    private $host = 'localhost';
+    private $dbName = 'app_lista_tarefas';
+    private $user = 'root';
+    private $pass = '';
+
+    public function connect()
+    {
+        try {
+
+            $connection = new PDO(
+                "mysql:host=$this->host,dbname=$this->dbName",
+                "$this->user",
+                "$this->pass",
+            );
+
+            return $connection;
+
+        } catch (PDOException $e) {
+            echo '<p>'. $e->getMessage() .'</p>';
+        }
+    }
+}
---
+++ lista_tarefas_private/TarefaService.php
@@ -0,0 +1,24 @@
+<?php
+
+class TarefaService
+{
+    public function insert()
+    {
+
+    }
+
+    public function recover()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+        
+    }
+}
